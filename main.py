1ce027df1d7350053a50dd8a75c7f026beebe961
Fredohm <fredwaucquez@hotmail.fr>
2022-04-07 07:16:55 +0200

    Create new cards

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import pandas
+from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+df = pandas.read_csv("./data/french_words.csv")
+word_dict = df.to_dict(orient="records")
+
+
+def generate_new_card():
+    current_card = choice(word_dict)
+    flash_card.itemconfig(card_title, text="French")
+    flash_card.itemconfig(card_word, text=current_card["French"])
+
 
 # UI SETUP
 window = Tk()
@@ -13,14 +25,17 @@ right_btn_img = PhotoImage(file="./images/right.png")
 wrong_btn_img = PhotoImage(file="./images/wrong.png")
 flash_card = Canvas(width=810, height=530, highlightthickness=0, bg=BACKGROUND_COLOR)
 flash_card.create_image(411, 273, image=card_front_img)
-title_text = flash_card.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-word_text = flash_card.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
-right_btn = Button(image=right_btn_img, highlightthickness=0)
-wrong_btn = Button(image=wrong_btn_img, highlightthickness=0)
+card_title = flash_card.create_text(400, 150, font=("Ariel", 40, "italic"))
+card_word = flash_card.create_text(400, 263, font=("Ariel", 60, "bold"))
+right_btn = Button(image=right_btn_img, highlightthickness=0, command=generate_new_card)
+wrong_btn = Button(image=wrong_btn_img, highlightthickness=0, command=generate_new_card)
 
 flash_card.grid(row=0, column=0, columnspan=2)
 wrong_btn.grid(row=1, column=0)
 right_btn.grid(row=1, column=1)
+
+
+generate_new_card()
 
 
 window.mainloop()
